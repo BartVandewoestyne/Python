4e2547f021f970a5d92ff6fe292ddba3b963fc46
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-09-21 09:43:02 +0200

    Add example on how to split long strings.

---
+++ examples/long_strings.py
@@ -0,0 +1,12 @@
+# References:
+#
+#   [1] https://docs.python.org/2.7/reference/lexical_analysis.html#literals (Python 2.7)
+#   [2] https://docs.python.org/3/reference/lexical_analysis.html#string-literal-concatenation (Python 3)
+
+s1 = ("this is a very "
+      "long string")
+print s1
+
+s2 = "this is another " \
+     "very long string"
+print s2
