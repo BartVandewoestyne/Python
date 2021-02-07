49ee45770c34e05e363004b30fbfbd5a60c3a88c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-02-07 23:53:39 +0100

    Draw using line segments between points.
    This prepares the script for plotting with X and Y displacements.

--- PyX/meshgrid.py
+++ PyX/meshgrid.py
@@ -7,10 +7,10 @@
 #   xpdf meshgrid.pdf (and select 'Full screen')
 #
 # TODO:
-#   * Allow X and Y displacements.
-#   * Load meshgrid from XML-file.
+#   * Load X and Y displacements and resolution from XML-file.
 
 from pyx import *
+import random
 
 unit.set(defaultunit="pt")
 
@@ -22,13 +22,35 @@ height = 1600
 deltax = width/(ncols-1)
 deltay = height/(nrows-1)
 
+nbpoints = nrows*ncols
+xcoords = [0]*nbpoints
+ycoords = [0]*nbpoints
+
+for row in range(0, nrows):
+    for col in range(0, ncols):
+        idx = row*nrows + col
+        factor = 0.5  # determines how much deformation of the grid we have.
+        xdisplacement = factor*random.uniform(-deltax/2, deltax/2)
+        ydisplacement = factor*random.uniform(-deltay/2, deltay/2)
+        xcoords[idx] = col*deltax + xdisplacement
+        ycoords[idx] = row*deltay + ydisplacement
+
 c = canvas.canvas()
 
 linestyle = [style.linewidth.THICK, color.rgb.blue]
 
-for row in range(0,nrows):
-    c.stroke(path.line(0, row*deltay, width, row*deltay), linestyle)
-for col in range(0,ncols):
-    c.stroke(path.line(col*deltax, 0, col*deltax, height), linestyle)
+# Draw 'horizontal' line segments.
+for row in range(0, nrows):
+    for col in range(0, ncols-1):
+        idxStart = row*nrows + col
+        idxEnd = idxStart + 1
+        c.stroke(path.line(xcoords[idxStart], ycoords[idxStart], xcoords[idxEnd], ycoords[idxEnd]), linestyle)
+
+# Draw 'vertical' line segments.
+for row in range(0,  nrows-1):
+    for col in range(0, ncols):   
+        idxStart = row*nrows + col
+        idxEnd = (row+1)*nrows + col
+        c.stroke(path.line(xcoords[idxStart], ycoords[idxStart], xcoords[idxEnd], ycoords[idxEnd]), linestyle)
 
 c.writePDFfile("meshgrid")
