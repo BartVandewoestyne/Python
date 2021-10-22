9add88babb2dac05139196297771b33e412d38e5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-10-22 21:21:09 +0200

    Minor change.

--- vaccination_efficiency.py
+++ vaccination_efficiency.py
@@ -16,8 +16,22 @@ total_gevaxt = gevaxt_vlaanderen + gevaxt_brussel + gevaxt_wallonie
 total_niet_gevaxt = niet_gevaxt_vlaanderen + niet_gevaxt_brussel + niet_gevaxt_wallonie
 total = total_gevaxt + total_niet_gevaxt
 
+# Dit klopt natuurlijk niet, want er is sprake van "per 100.000 personen in elke groep"...
+# Wat bedoelt men daar juist mee???  Hoe groot is elke groep???
+print("total number of fully vaxed people: ", total_gevaxt)
+print("total number of not vaxed people: ", total_niet_gevaxt)
+print("total number of people: ", total)
+
 percentage_gevaxt = total_gevaxt / total
 percentage_niet_gevaxt = total_niet_gevaxt / total
+
+# Output is currently:
+#   D:\GIT\Python>py -3 vaccination_efficiency.py
+#   percentage gevaxt =  0.2225115184005192
+#   percentage niet gevaxt =  0.7774884815994809
+# but on
+#   https://www.demorgen.be/nieuws/niet-gevaccineerden-tot-tien-keer-vaker-in-ziekenhuis-opgenomen-dan-gevaccineerden~b1cbd29b/
+# it is mentioned that 55 percent was fully vaccinated...
 
 print("percentage gevaxt = ", percentage_gevaxt)
 print("percentage niet gevaxt = ", percentage_niet_gevaxt)
